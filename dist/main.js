fe25ef2eb8126779f57766ec1752c5ea05887b18
voduyquocthai <voduyquocthai@gmail.com>
2021-04-29 11:40:57 +0700

    first commit

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Rectangle_1 = require("./Rectangle");
+const rectangle = new Rectangle_1.Rectangle(5, 5, 10, 20);
+console.log(rectangle.toString());
+console.log(rectangle.area());
+//# sourceMappingURL=main.js.map
